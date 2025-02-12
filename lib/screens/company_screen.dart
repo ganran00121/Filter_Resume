5f0e0c05a959e5950df649caf16019e55990af85
kaprowl <aucarapon.m1@gmail.com>
2025-02-12 21:08:16 +0700

    navbar + home screen

---
+++ lib/screens/company_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompanyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('🏢 Company Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
